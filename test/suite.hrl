8249a0b0c2b16699fac3f9d9e644bc9cef43e41e
Kostiantyn Povietkin <kostyantyn.povietkin@nextiva.com>
2019-09-03 15:37:54 +0300

    build process update

--- test/suite.hrl
+++ test/suite.hrl
@@ -1,5 +1,5 @@
 -include_lib("common_test/include/ct.hrl").
--include_lib("fast_xml/include/fxml.hrl").
+-include_lib("fast_xml/include/-include_lib("fast_xml/include/fxml.hrl").").
 -include("ns.hrl").
 -include("mod_proxy65.hrl").
 -include("xmpp_codec.hrl").
